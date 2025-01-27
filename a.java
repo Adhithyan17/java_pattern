ccfc9f5e4cc07e35e5578d6606ed4f633665d68b
Adhithyan17 <adhithyan1702@gmail.com>
2025-01-27 18:59:53 +0530

    first commit

---
+++ a.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class X{
+    public static void main(String srg[]){
+
+        String str="PROGRAM";
+        for(int i=0;i<str.length();i++){
+            int len = str.length();
+            for(int j=0;j<len;j++){
+                if(i==j || j==len-i-1){
+                    System.out.print(str.charAt(j));
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
